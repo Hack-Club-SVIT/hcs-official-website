f3bb51a75f9b2fc514b5e23048c3aa59223e54f2
VIVEK <sutharvivek18@gmail.com>
2022-08-19 12:22:55 +0530

    fea : vite+preact+tailwindcss setup done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <nav className="flex flex-row h-16 w-screen justify-between bg-violet-500">
+      <div className="flex-[0.2]  p-4 flex justify-center">Logo</div>
+      <div className="flex-[0.5]  p-4 flex justify-center">Opiton</div>
+      <div className="flex-[0.2]  p-4 flex justify-center">Join</div>
+    </nav>
+  );
+}
