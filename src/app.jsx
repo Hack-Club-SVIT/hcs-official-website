fee0bf42858786fde364897a181fd9115b36065b
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2022-08-31 09:59:18 +0530

    fea : about added

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,13 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
 
 export function App() {
-
   return (
     <>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <About />
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+function About() {
+  return (
+    <section class="bg-[#2c2c2c]">
+      {/* <div>About Us</div> */}
+      <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+        <img
+          class="w-full hidden dark:block rounded-lg"
+          src="/team.png"
+          alt="dashboard image"
+        />
+        <div class="mt-4 md:mt-0">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-primary text-center">
+            Who are we ?
+          </h2>
+          <p class="mb-6 font-light text-white md:text-2xl  text-center">
+            Hack Club SVIT is Community of passionate coders,students who are
+            curios to learn and whos belive in mutual growth and want to learn
+            in public
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
